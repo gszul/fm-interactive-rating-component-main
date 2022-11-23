13be681ca22cc57965fe4ce505b497787831eccf
Grzegorz Szul <g.szul00@gmail.com>
2022-11-23 20:20:20 +0100

    added thanks-state

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const rateState = document.getElementById('rate-state');
+const thanksState = document.getElementById('thanks-state');
+const rate = document.getElementById('rate');
+
 const displayThanks = (ev) => {
     ev.preventDefault();
     
-    console.log(ev.target.rate.value);
+    rate.innerText = ev.target.rate.value;
+    rateState.classList.add('transparent');
+    thanksState.classList.remove('transparent');
 }
